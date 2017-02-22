fb8d6b68be646f57a664b10c6a7fd1e527bf97de
Tom on AWS <tom.tom.seddon@gmail.com>
2017-02-22 17:40:20 +0000

    finished hospital variability

--- exercise_1/investigations/hospital_variability/proc_variability.sql
+++ exercise_1/investigations/hospital_variability/proc_variability.sql
@@ -2,13 +2,13 @@ DROP TABLE procedure_var;
 
 CREATE TABLE procedure_var AS
 SELECT
-  sd_ami/avg_ami AS ami_var,
-  sd_hf/avg_hg AS hf_var,
-  sd_pn/avg_pn as pn_var,
-  sd_preg/avg_preg as preg_var,
-  sd_imm/avg_imm as imm_var,
-  sd_er/avg_er as er_var,
-  sd_psi/avg_psi as psi_var
+  cast(sd_ami/avg_ami AS decimal(4,3)) ami_var,
+  cast(sd_hf/avg_hf AS decimal(4,3)) hf_var,
+  cast(sd_pn/avg_pn as decimal(4,3)) pn_var,
+  cast(sd_preg/avg_preg as decimal(4,3)) preg_var,
+  cast(sd_imm/avg_imm as decimal(4,3)) imm_var,
+  cast(sd_er/avg_er as decimal(4,3)) er_var,
+  cast(sd_psi/avg_psi as decimal(4,3)) psi_var
 FROM
   summary_variables
 ;
