f933772cf930f749d9cf18e4e7db5901ae48c989
Tom on AWS <tom.tom.seddon@gmail.com>
2017-02-22 17:23:51 +0000

    finished states section

--- exercise_1/investigations/best_states/best_states.sql
+++ exercise_1/investigations/best_states/best_states.sql
@@ -2,13 +2,21 @@ DROP TABLE totalled_states;
 
 CREATE TABLE totalled_states AS
 SELECT
-  hospital_selected.state
-  count(scored_hosps.provider_id) as hosp_count,
-  avg(scored_hosps.z_total) as avg_rating
+  hospital_selected.state,
+  cast(avg(totalled_hosps.z_total) as decimal(3,2)) avg_rating,
+  count(totalled_hosps.provider_id) as hosp_count
 FROM
-  hospital_selected
-  scored_hosps
+  hospital_selected,
+  totalled_hosps
 WHERE
-  hospital_selected.provider_id=scored_hosps.provider_id
+  hospital_selected.provider_id=totalled_hosps.provider_id
 GROUP BY
   hospital_selected.state
+;
+
+SELECT *
+FROM totalled_states
+WHERE hosp_count>=20
+ORDER BY avg_rating DESC
+LIMIT 10
+;
