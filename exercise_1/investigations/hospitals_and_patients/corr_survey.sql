7f80091c76b84e3ab0ab2a2b95a2b44e45f88f5b
Tom on AWS <tom.tom.seddon@gmail.com>
2017-02-24 17:00:25 +0000

    finished correlation section

--- exercise_1/investigations/hospitals_and_patients/corr_survey.sql
+++ exercise_1/investigations/hospitals_and_patients/corr_survey.sql
@@ -1,5 +1,14 @@
-DROP TABLE simple_rating;
+-- Script to identify how patient survey responses are linked to other quality measures
+-- It looks at it two ways.
 
+-- Script developed to run in SparkSQL.
+
+-- Note: Assumes best_hospitals.sql script has already been run.
+
+
+-- Create combination of patient survey score and overall HVBP hospital rating
+
+DROP TABLE simple_rating;
 CREATE TABLE simple_rating AS
 SELECT
   hospital_selected.overall_rating,
@@ -13,6 +22,9 @@ AND overall_rating not like "Not%"
 AND overall_performance not like "Not%" 
 ;
 
+-- Calculate average survey rating by HVBP rating group
+DROP TABLE survey_hvbp;
+CREATE TABLE survey_hvbp AS
 SELECT
   overall_rating,
   cast(AVG(overall_performance) AS decimal(4,2)) avg_perf,
@@ -21,6 +33,10 @@ FROM simple_rating
 GROUP BY overall_rating
 ORDER BY overall_rating DESC
 ;
+
+
+-- Create combination of patient survey rating and my normalized, weighted quality
+-- score developed to find the best hospitals
 
 DROP TABLE proc_rating;
 CREATE TABLE proc_rating AS
@@ -35,6 +51,9 @@ WHERE
 AND overall_achieve_pts not like "Not%"
 ;
 
+-- Calculate average quality score by patient survey score group
+DROP TABLE survey_zrating;
+CREATE TABLE survey_zrating AS
 SELECT
   overall_achieve_pts,
   cast(AVG(z_total) as decimal(3,2)) avg_z,
---
+++ exercise_1/investigations/hospitals_and_patients/readme.txt
@@ -0,0 +1,6 @@
+Script and support for the investigating how patient responses are correlated
+to hospital rating scores.
+
+Script developed to run in SparkSQL.
+
+Note:  Assumes best_hospitals.sql script has already been run.
