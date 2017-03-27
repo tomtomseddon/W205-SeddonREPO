17c7d5717b8dfd91f4122778a1b071b536524fe0
w205 <w205@ip-172-31-5-71.ec2.internal>
2017-03-27 14:58:53 +0000

    added credentials

--- exercise_2/Twittercredentials.py
+++ exercise_2/Twittercredentials.py
@@ -1,16 +1,16 @@
 import tweepy
 
-consumer_key = "YourConsumerkey";
+consumer_key = "GYMBVqR7pJJYgVimhmafomtY4";
 #eg: consumer_key = "YisfFjiodKtojtUvW4MSEcPm";
 
 
-consumer_secret = "YourConsumerSecret";
+consumer_secret = "9c8kMWgP23RWWh4tCdpuRF5kRfKvH7M3Bliqe8LTOPkDUjG2NG";
 #eg: consumer_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token = "YourAcessToken";
+access_token = "330338366-AJAeQnh2EPUuc9abN3aa45lqhRqpocyrl7UbghQs";
 #eg: access_token = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
-access_token_secret = "YourTokenSecret";
+access_token_secret = "AhYI6wsL1K2u64lg0DYnZVAln3Mqh3xLZdkC6uJ9zXqji";
 #eg: access_token_secret = "YisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPmYisfFjiodKtojtUvW4MSEcPm";
 
 
